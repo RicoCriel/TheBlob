d516a2fadbb1ee1248537cf419d7c1b7bd9ce56d
RicoCriel <rico.criel@hotmail.be>
2024-03-05 10:09:27 +0100

    Piece logic start

---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PieceState
+{
+    Dead,
+    Blobbed,
+    Hostile
+}
+
+public enum PieceType
+{
+    JustABlob,
+    Car,
+    Building
+}
+
+public class Piece : MonoBehaviour
+{
+    
+}
