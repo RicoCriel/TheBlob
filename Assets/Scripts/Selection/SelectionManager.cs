6a6215d0ed5fc452edb31eea4521ae005ccd0870
Warre Van Tittelboom <warrewarmanske@gmail.com>
2024-03-06 16:19:18 +0100

    Basic Movement Finally works ;_;

--- Assets/Scripts/Selection/SelectionManager.cs
+++ Assets/Scripts/Selection/SelectionManager.cs
@@ -5,14 +5,21 @@ namespace SelectionBlob
 {
     public class SelectionManager
     {
+        static public Vector2[] _directions =
+            new Vector2[4]
+            {
+                new Vector2(1,  0), new Vector2(0, 1), new Vector2(0,-1),
+                new Vector2(-1,  0)
+            };
         //normal movement which is basically BFS range 1;
         public List<Tile> GetTilesNormalMovement(Tile center, GridManager gridManager)
         {
             List<Tile> foundtiles = new List<Tile>();
 
-            foreach (Vector3 direction in _directions)
+            foreach (Vector2 direction in _directions)
             {
-                if (gridManager.TryGetTileAt(center.transform.position + direction, out Tile tile))
+                Debug.Log(new Vector2(center.transform.position.x, center.transform.position.z) + direction);
+                if (gridManager.TryGetTileAt(new Vector2(center.transform.position.x, center.transform.position.z) + direction, out Tile tile))
                 {
                     // if (gridManager.TryGetPieceAt(tile, out Piece piece))
                     // {
@@ -55,16 +62,16 @@ namespace SelectionBlob
         {
             List<Tile> foundtiles = new List<Tile>();
 
-            foreach (var VARIABLE in _directions)
-            {
-                if (grid.TryGetTileAt(centertile.transform.position + VARIABLE, out Tile tile))
-                {
-                    if (!grid.TryGetPieceAt(tile, out Piece piece))
-                    // {
-                        foundtiles.Add(tile);
-                    // }
-                }
-            }
+            //foreach (var VARIABLE in _directions)
+            //{
+            //    if (grid.TryGetTileAt(centertile.transform.position + VARIABLE, out Tile tile))
+            //    {
+            //        if (!grid.TryGetPieceAt(tile, out Piece piece))
+            //        // {
+            //            foundtiles.Add(tile);
+            //        // }
+            //    }
+            //}
 
             return foundtiles;
         }
@@ -74,12 +81,7 @@ namespace SelectionBlob
             return Mathf.Abs(start.x - goal.x) + Mathf.Abs(start.z - goal.z);
         }
 
-        static public Vector3[] _directions =
-            new Vector3[4]
-            {
-                new Vector3(1, 0, 0), new Vector3(1, 0, -1), new Vector3(0, 0, -1),
-                new Vector3(-1, 0, 0)
-            };
+        
 
     }
 
